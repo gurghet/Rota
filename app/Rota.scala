2c073da331a23ed31d847a3f6ae0a1df951828c1
Andrea Passaglia <gurghet@cucchigatto.local>
2016-04-11 00:43:11 +0200

    working

--- app/Rota.scala
+++ app/Rota.scala
@@ -1,3 +1,5 @@
+import rx.lang.scala.Observable
+
 import scala.collection.immutable.IndexedSeq
 import scala.collection.mutable
 
@@ -8,6 +10,13 @@ class Rota(nDays: Int, team: Set[WorkerId]) {
   private val shiftsPerDay = 3
   private val maxGlobalTeamSize = 5
   private val r = scala.util.Random
+  type MutableRota = mutable.LinkedHashMap[(Int, Int), mutable.Set[WorkerId]]
+  type Rota = Map[(Int, Int), Set[WorkerId]]
+  type ImmutableRota = collection.immutable.Map[(Int, Int), collection.immutable.Set[WorkerId]]
+
+  private var lastAdd: Option[((WorkerId, mutable.Set[WorkerId]))] = Option.empty
+  private var lastDel: Option[((WorkerId, mutable.Set[WorkerId]))] = Option.empty
+
 
   // all the shifts have an implicit day and order
   // for example the with 3 shifts per day the 4th
@@ -22,22 +31,28 @@ class Rota(nDays: Int, team: Set[WorkerId]) {
   /**
     * Write an initial random solution
     */
-  def init() {
+  def init(point: Option[Rota] = Option.empty) {
 
-    // calculations are 1-indexed
-    allShifts
-      .foreach{ case (day, shift) =>
-        val currentShiftTeam = mutable.HashSet.empty[WorkerId]
-        val availableWorkers = team.toBuffer
-        for (i <- 1 to r.nextInt(math.min(maxGlobalTeamSize, team.size)) + 1) {
-          val randomWorker: Int = r.nextInt(availableWorkers.size)
-          currentShiftTeam.add(availableWorkers.remove(randomWorker))
-        }
+    if (point.isEmpty) {
+      // calculations are 1-indexed
+      allShifts
+        .foreach { case (day, shift) =>
+          val currentShiftTeam = mutable.HashSet.empty[WorkerId]
+          val availableWorkers = team.toBuffer
+          for (i <- 1 to r.nextInt(math.min(maxGlobalTeamSize, team.size)) + 1) {
+            val randomWorker: Int = r.nextInt(availableWorkers.size)
+            currentShiftTeam.add(availableWorkers.remove(randomWorker))
+          }
           rota += (day, shift) -> currentShiftTeam
+        }
+    } else {
+      point.get.foreach{case ((day, shift), equipe) =>
+        rota += (day, shift) -> (mutable.Set() ++ equipe)
+      }
     }
   }
 
-  def get(): collection.immutable.Map[(Int, Int), collection.immutable.Set[WorkerId]] = {
+  def get(): ImmutableRota = {
     allShifts
       .map{ case (day, shift) =>
         (day, shift) -> rota(day, shift).toSet
@@ -47,54 +62,108 @@ class Rota(nDays: Int, team: Set[WorkerId]) {
   def randomShift = (r.nextInt(nDays) + 1, r.nextInt(shiftsPerDay) + 1)
 
   def drawRandomWorkerFromRandomShift(): Option[WorkerId] = {
-    val randomShift1: (Int, Int) = randomShift
-    var shift = rota(randomShift1)
+    var shift = randomShift
+    var equipe = rota(shift)
     var trial = 1; val maxTrials = 5
-    while (shift.isEmpty || trial > maxTrials) {
+    while (equipe.isEmpty && trial <= maxTrials) {
       trial += 1
-      shift = rota(randomShift)
+      shift = randomShift
+      equipe = rota(shift)
     }
-    if (shift.isEmpty) {
+    if (equipe.isEmpty) {
+      lastDel = Option.empty
       Option.empty[WorkerId]
     } else {
-      val teamSize = shift.size
-      val pickedWorker = shift.iterator.drop(r.nextInt(teamSize)).next
-      shift.remove(pickedWorker)
+      val teamSize = equipe.size
+      val pickedWorker = equipe.iterator.drop(r.nextInt(teamSize)).next
+      equipe.remove(pickedWorker)
+      //print(s" $shift->${pickedWorker.id} ")
+      lastDel = Option((pickedWorker, equipe))
       Option(pickedWorker)
     }
   }
 
-  def addWorkerToRandomShift(worker: WorkerId) {
-    val shift = rota(randomShift)
-    shift += worker
+  def unDrawRandomWorkerFromRandomShift() {
+    if (lastDel.isDefined) {
+      val shift = lastDel.get._2
+      shift += lastDel.get._1
+    }
   }
 
-  def swap() {
-    val maybeWorker = drawRandomWorkerFromRandomShift()
-
-    if (maybeWorker.isDefined) {
-      val worker = maybeWorker.get
-      var shift = rota(randomShift)
-      // if the worker is already present take another shift
-      while (shift.contains(worker)) {
-        shift = rota(randomShift)
-      }
-      shift += worker
+  def addWorkerToRandomShift(worker: WorkerId) {
+    var shift = randomShift
+    var equipe = rota(shift)
+    var trial = 1; val maxTrials = 5
+    // if the worker is already present take another shift
+    while (equipe.contains(worker) && trial <= maxTrials) {
+      trial += 1
+      shift = randomShift
+      equipe = rota(shift)
+    }
+    if (!equipe.contains(worker)) {
+      //print(s" ${worker.id}->$shift ")
+      lastAdd = Option((worker, equipe))
+      equipe += worker
+    } else {
+      lastAdd = Option.empty
     }
 
   }
 
+  def unAddWorkerToRandomShift(): Unit = {
+    if (lastAdd.isDefined) {
+      val shift = lastAdd.get._2
+      shift -= lastAdd.get._1
+    }
+  }
+
+  def movingRandomWorkerSomewhereElse() {
+    val maybeWorker = drawRandomWorkerFromRandomShift()
+
+    if (maybeWorker.isDefined) {
+      val worker = maybeWorker.get
+      addWorkerToRandomShift(worker)
+    } else {
+      unDrawRandomWorkerFromRandomShift()
+    }
+    //println("moving")
+  }
+
+  def unMovingRandomWorkerSomewhereElse() {
+    //println("undo moving")
+    unAddWorkerToRandomShift()
+    unDrawRandomWorkerFromRandomShift()
+  }
+
   def addRandomWorkerToRandomShift() {
+    //println("adding")
     val randomWorker: WorkerId = team.iterator.drop(r.nextInt(team.size)).next
     addWorkerToRandomShift(randomWorker)
   }
 
+  def unAddRandomWorkerToRandomShift() {
+    //println("undo adding")
+    unAddWorkerToRandomShift()
+  }
+
   def removeRandomWorkerFromRandomShift() {
+    //println("removing")
     drawRandomWorkerFromRandomShift()
   }
 
-  def pickNeighborhood(): () => () => Unit = {
-    val neighborhoods = Set(removeRandomWorkerFromRandomShift _, addRandomWorkerToRandomShift _, swap _)
+  def unRemoveRandomWorkerFromRandomShift() {
+    //println("undo removing")
+    unDrawRandomWorkerFromRandomShift()
+  }
+
+  case class Move(f: () => Unit, `f⁻¹`: () => Unit)
+
+  def pickNeighborhood(): Move = {
+    val neighborhoods = Set(
+      Move(removeRandomWorkerFromRandomShift, unRemoveRandomWorkerFromRandomShift),
+      Move(addRandomWorkerToRandomShift, unAddRandomWorkerToRandomShift),
+      Move(movingRandomWorkerSomewhereElse, unMovingRandomWorkerSomewhereElse)
+    )
     neighborhoods.iterator.drop(r.nextInt(neighborhoods.size)).next
   }
 
@@ -117,9 +186,68 @@ class Rota(nDays: Int, team: Set[WorkerId]) {
       }.toList
   }
 
-  def loss(): Int = {
-    val point = get
+  def simplePresencesToLoss(presences: List[Int]): Int = {
+    var retVal = 0
+    Observable.from(presences)
+      .sliding(3, 1)
+      .map(_.sum)
+      .flatten
+      .map(sum => if (sum == 0 || sum == 1) 0 else sum * -2 + 2)
+      .sum
+      .subscribe {
+        result => retVal = result
+      }
+    retVal
+  }
 
+  def gain(): Double = {
+    val point = get
+    var sum = 0D
+    for (w <- team) {
+      val preferences = workerPreferences.getOrElse(w, List.fill(nDays * shiftsPerDay)(0))
+      val presences = getPresenceListFor(w)
+      sum += preferences.zip(presences).map{case (preference, presence) => preference * presence}.sum
+      sum += simplePresencesToLoss(presences)
+    }
+
+    // controllo delle proprietà locali
+    val props = shiftProperties.zip(rota)
+    for (s <- props) {
+      val equipe = s._2._2
+      val shift = s._2._1
+      val props = s._1
+      var idealEquipeSize = 1
+      if (props.contains("mattino") && !props.contains("vacanza")) {
+        idealEquipeSize = 4
+      }
+      if (props.contains("altroRep")) {
+        idealEquipeSize = 0
+      }
+      sum -= math.pow(idealEquipeSize - equipe.size, 2)
+    }
+    // controllo in un intorno di 5 giorni
+    // controllo proprietà globali
+
+    sum
+  }
+
+
+
+  def go(maxMoves: Int = 1000): ImmutableRota = {
+    var currentGain = gain
+    for (i <- 1 to maxMoves) {
+      val move = pickNeighborhood
+      move.f()
+      val newGain = gain
+      if (newGain < currentGain) {
+        //println(s"gain no better than $currentGain, will undo")
+        move.`f⁻¹`()
+      } else {
+        currentGain = newGain
+        //println(s"gain $currentGain->$newGain!")
+      }
+    }
+    get
   }
 }
 
--- test/RotaSpec.scala
+++ test/RotaSpec.scala
@@ -4,23 +4,88 @@ import org.scalatest.{FlatSpec, Matchers}
   * Created by Andrea on 06/04/2016.
   */
 class RotaSpec extends FlatSpec with Matchers {
+  def smallExampleRota: Rota = new Rota(1, Set(WorkerId(1), WorkerId(2)))
+  def smallExampleRotaManyWorkers: Rota = new Rota(1, Set(WorkerId(1), WorkerId(2), WorkerId(3), WorkerId(4), WorkerId(5)))
+  def bigExampleRota: Rota =
+    new Rota(30, Set(WorkerId(1), WorkerId(2), WorkerId(3), WorkerId(4), WorkerId(5), WorkerId(6)))
+
   "Rota" should "non contain duplicate values" in {
-    val rota = new Rota(1, Set(WorkerId(1), WorkerId(2)))
+    val rota = smallExampleRota
     rota.init()
     rota.get() should contain noneOf (Set(WorkerId(1), WorkerId(1)), Set(WorkerId(2), WorkerId(2)))
   }
 
-  it should "correctly select a function" in {
-    val rota = new Rota(1, Set(WorkerId(1), WorkerId(2)))
-    rota.init()
-    rota.pickNeighborhood() shouldBe a [() => _]
-  }
-
   it should "swap two workers" in {
-    val rota = new Rota(30, Set(WorkerId(1), WorkerId(2), WorkerId(3), WorkerId(4), WorkerId(5), WorkerId(6)))
+    val rota = bigExampleRota
     rota.init()
     val rotaInit = rota.get
-    rota.swap()
+    rota.movingRandomWorkerSomewhereElse()
     rota.get should not equal rotaInit
+  }
+
+  it should "unswap two workers" in {
+    val rota = bigExampleRota
+    rota.init()
+    val rotaInit = rota.get
+    rota.movingRandomWorkerSomewhereElse()
+    rota.unMovingRandomWorkerSomewhereElse()
+    rota.get shouldEqual rotaInit
+  }
+
+  it should "return a solution" in {
+    val rota = smallExampleRota
+    rota.init()
+    val rotaInit = rota.get
+    val rotaFinal = rota.go()
+    rotaFinal should not equal rotaInit
+  }
+
+  it should "correctly classify two patterns of presence" in {
+    val ex = smallExampleRota
+    val good = ex.simplePresencesToLoss(List(0,0,0))
+    val equallyGood1 = ex.simplePresencesToLoss(List(0,0,0,1,0,0,0))
+    val equallyGood2 = ex.simplePresencesToLoss(List(0,1,0,0,1))
+    val bad = ex.simplePresencesToLoss(List(1,0,1,0,0,0,0))
+    val worse = ex.simplePresencesToLoss(List(0,0,1,1,0,0,0))
+    val superWorse = ex.simplePresencesToLoss(List(1,1,1))
+    good shouldEqual equallyGood1
+    equallyGood2 shouldEqual equallyGood1
+    bad shouldBe > (worse)
+    worse shouldBe > (superWorse)
+  }
+
+  it should "correctly classify two patterns of preferences" in {
+    val good = smallExampleRota
+    val bad = smallExampleRota
+    good.setPreferencesFor(WorkerId(1), Map((1, 1) -> -4, (1, 2) -> 0, (1, 3) -> 2))
+    good.setPreferencesFor(WorkerId(2), Map((1, 1) -> 4, (1, 2) -> -2, (1, 3) -> 0))
+    good.init(Option(Map((1, 1) -> Set(WorkerId(2)), (1, 2) -> Set(WorkerId(1)), (1, 3) -> Set(WorkerId(1)))))
+    bad.setPreferencesFor(WorkerId(1), Map((1, 1) -> -4, (1, 2) -> 0, (1, 3) -> 2))
+    bad.setPreferencesFor(WorkerId(2), Map((1, 1) -> 4, (1, 2) -> -2, (1, 3) -> 0))
+    bad.init(Option(Map((1, 1) -> Set(WorkerId(1)), (1, 2) -> Set(WorkerId(1)), (1, 3) -> Set(WorkerId(2)))))
+    good.gain shouldBe > (bad.gain)
+  }
+
+  it should "size the shifts appropriately" in {
+    val r = smallExampleRotaManyWorkers
+    val shiftProps =
+      Map((1, 1) -> Set("mattino"), (1, 2) -> Set("pomeriggio"), (1, 3) -> Set("altroRep"))
+    r.setShiftProperties(shiftProps)
+    r.init()
+    println(s"Init     ${r.get}")
+    println(s"Init     $shiftProps")
+    val solution = r.go(200)
+    println(s"Solution $solution")
+    solution((1, 1)).size shouldBe 4
+    solution((1, 2)).size shouldBe 1
+    solution((1, 3)).size shouldBe 0
+  }
+
+  it should "work" in {
+    val example = smallExampleRota
+    example.init()
+    println(s"Init     ${example.get}")
+    val solution = example.go()
+    println(s"Solution $solution")
   }
 }
